187124650444708134a3fb8b5deef596f38109f1
Lucasms29 <covalske9@gmail.com>
2022-05-25 14:22:06 -0300

    new test

--- Projeto pb/cucumber e capybara/features/specs/google.feature
+++ Projeto pb/cucumber e capybara/features/specs/google.feature
@@ -5,3 +5,8 @@ Funcionalidade: Google
 Cenário: Carregar a página do Google
     Dado que acesse a home
     Então deverá exibir o logo do Google
+
+Cenário: Buscar por Compass
+    Dado que acesse a home
+    Quando buscar por "Compass"
+    Então deverão ser encontrados resultados
--- Projeto pb/cucumber e capybara/features/step_definitions/google.rb
+++ Projeto pb/cucumber e capybara/features/step_definitions/google.rb
@@ -5,5 +5,13 @@ Dado('que acesse a home') do
   Então('deverá exibir o logo do Google') do
     expect(page).to have_content 'Gmail'
     expect(page).to have_css '.lnXdpd'
-    .lnXdpd
+  end
+
+  Quando('buscar por {string}') do |termo|
+    find("input[name='q']").set termo
+    find(".gNO89b").click
+  end
+  
+  Então('deverão ser encontrados resultados') do
+    expect(page).to have_content 'Jeep Compass'
   end
