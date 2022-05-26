485800cc6c4067af8ef3a3aa80388ddc908da154
Lucasms29 <covalske9@gmail.com>
2022-05-26 15:58:23 -0300

    add headless

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -10,5 +10,17 @@ ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENV
 puts ENVIRONMENT_CONFIG
 URL = ENVIRONMENT_CONFIG['url']
 
-Capybara.default_driver = :selenium_chrome	
+Capybara.register_driver :my_chrome do |app|
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => 
+        {"args" => ["--incognito", "--start-maximized", "--window-size=1420,835"]})
+        
+    if ENV['HEADLESS']
+         caps['goog:chromeOptions']['args'] << '--headless'
+    end
+    
+    options = { browser: :chrome, desired_capabilities: caps}
+    Capybara::Selenium::Driver.new(app, options)
+end
+
+Capybara.default_driver = :my_chrome
 Capybara.app_host = URL
