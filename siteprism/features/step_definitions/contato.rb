f0ef468e5ed871d2d35d9cd8464966a0e72e53b3
Lucasms29 <covalske9@gmail.com>
2022-06-01 15:13:08 -0300

    massa de dados dinamica3

--- siteprism/features/step_definitions/contato.rb
+++ siteprism/features/step_definitions/contato.rb
@@ -1,8 +1,9 @@
 Quando('entrar em contato pelo formulário da home') do
-    binding.pry
+    contact_data = Factory::Dynamic.message_for_contact
+    @home_page.send_message_contact_us(contact_data)
 end
 
 
 Então ('deverá apresentar um modal de mensagem enviada') do
-    binding.pry
+    expect(@home_page).to have_success_message
 end 
--- siteprism/features/support/factory/static/dynamic.rb
+++ siteprism/features/support/factory/static/dynamic.rb
@@ -4,10 +4,10 @@ module Factory
 
     class Dynamic
 
-        def self.user_for_registering
+        def self.message_for_contact
             {
                 email: Faker::Internet.email,
-                message: Faker::Lorem.unique
+                message: Faker::Lorem.paragraph
             }
         end
 
