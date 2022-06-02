64cc1c2a5d22e32860f429f71033c553671c83d3
Lucasms29 <covalske9@gmail.com>
2022-06-02 13:38:31 -0300

    add screenshot

--- siteprism/features/support/hooks.rb
+++ siteprism/features/support/hooks.rb
@@ -1,12 +1,7 @@
-Before do 
-    puts 'Estou executando antes de cada cenário'
-end
-
 After do |scenario|
-    puts 'Estou executando depois'
-    puts scenario.failed?
+    if scenario.failed?
+        screenshot = Capybara.page.save_screenshot('reports/screenshots/sc.png')
+        attach(screenshot, 'image/png')
+    end
 end
 
-After ('@limpar_banco') do
-    puts 'EXECUTANDO DEPOIS DO CENÁRIO Carregar página do google 2'
-end
